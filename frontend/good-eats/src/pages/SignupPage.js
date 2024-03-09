0cacad32822216f260afc5dd7155145d109ba466
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-08 18:37:53 -0800

    Frontend add: Signup page username,password,and email validation

--- frontend/good-eats/src/pages/SignupPage.js
+++ frontend/good-eats/src/pages/SignupPage.js
@@ -10,8 +10,7 @@ class SignUpForm extends Component {
       email: '',
       password: '',
       confirmPassword: '',
-      error: '',
-      passwordValidation: {}
+      errors: {},
     };
   }
   
@@ -21,24 +20,67 @@ class SignUpForm extends Component {
       if (name === 'password' || name === 'confirmPassword') {
         this.validatePassword();
       }
+      if (name === 'email') {
+        this.validateEmail();
+      }
+      if (name === 'username') {
+        this.validateUsername();
+      }
     });
   };
 
+  validateUsername = () => {
+    const { username } = this.state;
+    const errors = {};
+
+    if (username.length < 2) {
+      errors.username = 'Username must be longer than 2 characters';
+    }
+    else if (/\s/.test(username)) {
+      errors.username = 'Username cannot contain spaces';
+    } 
+    else {
+      delete errors.username;
+    }
+
+    this.setState({errors});
+    
+  }
   validatePassword = () => {
     const { password, confirmPassword } = this.state;
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    const errors = {};
 
     if (!passwordRegex.test(password)) {
-      this.setState({ error: 'Password must contain at least one digit, one uppercase letter, one lowercase letter, and at least 6 characters' });
-      return false;
-    } else if (password !== confirmPassword) {
-      this.setState({ error: 'Passwords do not match' });
-      return false;
+      errors.password = 'Password must contain at least one digit, one uppercase letter, one lowercase letter, and at least 6 characters';
     } else {
-      this.setState({ error: '' });
-      return true;
+      delete errors.password;
     }
+
+    if (password !== confirmPassword) {
+      errors.confirmPassword = 'Passwords do not match';
+    } else {
+      delete errors.confirmPassword;
+    }
+
+    this.setState({ errors });
   };
+
+  validateEmail = () => {
+    const { email } = this.state;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const errors = {};
+
+    if (!emailRegex.test(email)) {
+      errors.email = 'Invalid Email Address';
+    } else {
+      delete errors.email;
+    }
+
+    this.setState({ errors });
+  };
+
+
 
   handleSubmit = async (e) => {
     e.preventDefault();
@@ -46,12 +88,31 @@ class SignUpForm extends Component {
     const { username, email, password, confirmPassword } = this.state;
 
     // Check if any field is empty
-    if (!username || !email || !password || !confirmPassword) {
-      this.setState({ error: 'Please fill out all fields' });
+    const errors = {};
+    if (!username) {
+      errors.username = 'Username is required';
+    }
+    if (!email) {
+      errors.email = 'Email is required';
+    }
+    if (!password) {
+      errors.password = 'Password is required';
+    }
+    if (!confirmPassword) {
+      errors.confirmPassword = 'Please confirm password';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      this.setState({ errors });
       return;
     }
 
-    if (!this.validatePassword()) {
+    this.validateUsername();
+    this.validatePassword();
+    this.validateEmail();
+
+    // errors object will only have keys if there are errors so if the length == 0 there are no errors
+    if (Object.keys(this.state.errors).length > 0) {
       return;
     }
 
@@ -67,26 +128,63 @@ class SignUpForm extends Component {
       console.log('User signed up successfully:', response.data);
 
       // Reset form fields
-      this.setState({ username: '', email: '', password: '', confirmPassword: '', error: '' });
+      this.setState({ username: '', email: '', password: '', confirmPassword: '', errors: {} });
     } catch (error) {
       // Handle error
       console.error('Error signing up:', error.response.data.message);
-      this.setState({ error: error.response.data.message });
+      /*
+      this is what the line of code below does   
+      errors: {
+        ...prevState.errors,
+      apiError: error.response.data.message
+      }
+  */
+      this.setState({ errors: { apiError: error.response.data.message } });
     }
   };
 
   render() {
-    const { username, email, password, confirmPassword, error } = this.state;
-    const emptyFieldStyle = { borderColor: 'red' }; // Define style for empty fields
-
+    const { username, email, password, confirmPassword, errors } = this.state;
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <InputField label="Username" type="text" name="username" value={username} onChange={this.handleChange} style={username ? {} : emptyFieldStyle} />
-          <InputField label="Email" type="email" name="email" value={email} onChange={this.handleChange} style={email ? {} : emptyFieldStyle} />
-          <InputField label="Password" type="password" name="password" value={password} onChange={this.handleChange} style={password ? {} : emptyFieldStyle} />
-          <InputField label="Reenter Password" type="password" name="confirmPassword" value={confirmPassword} onChange={this.handleChange} style={confirmPassword ? {} : emptyFieldStyle} />
-          {error && <div>Error: {error}</div>}
+          <InputField
+            label="Username"
+            type="text"
+            name="username"
+            value={username}
+            onChange={this.handleChange}
+            style={errors.username ? { borderColor: 'red' } : {}}
+          />
+          {errors.username && <div>{errors.username}</div>}
+          <InputField
+            label="Email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            style={errors.email ? { borderColor: 'red' } : {}}
+          />
+          {errors.email && <div>{errors.email}</div>}
+          <InputField
+            label="Password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            style={errors.password ? { borderColor: 'red' } : {}}
+          />
+          {errors.password && <div>{errors.password}</div>}
+          <InputField
+            label="Reenter Password"
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            style={errors.confirmPassword ? { borderColor: 'red' } : {}}
+          />
+          {errors.confirmPassword && <div>{errors.confirmPassword}</div>}
+          {errors.apiError && <div>{errors.apiError}</div>}
           <button type="submit">Register</button>
         </form>
       </div>
