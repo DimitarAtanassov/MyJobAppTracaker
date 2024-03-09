d119a3717be9096e373b2d3d88e6a9bd929464f4
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-09 15:28:04 -0800

    Frontend: Finished Login Page Sekeleton

--- frontend/good-eats/src/App.js
+++ frontend/good-eats/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignupPage from './pages/SignupPage';
-
+import LoginPage from './pages/LoginPage';
 function App() {
   return (
    <>
         <h1>User Sign-Up</h1>
         <SignupPage />
+        <hr/>
+        <h1>User Login</h1>
+        <LoginPage />
     </>
   );
 }
--- frontend/good-eats/src/pages/LoginPage.js
+++ frontend/good-eats/src/pages/LoginPage.js
@@ -8,6 +8,7 @@ class LoginPage extends Component {
         this.state = {
             username: '',
             password: '',
+            errors: {},
         };
     };
 
@@ -18,9 +19,22 @@ class LoginPage extends Component {
 
     handleSubmit = async (e) => {
         e.preventDefault();
+        const {username,password} = this.state;
+        const errors = {};
 
+        if(!username){
+            errors.username = "Username is required";
+        }
+        if(!password){
+            errors.password = "Password is required";
+        }
+        // Check if there is any errors
+        if (Object.keys(errors).length > 0) {
+            this.setState({errors});
+            return;
+        }
         try {
-            const respone = await axios.post('API ROUTE FOR LOGIN', {
+            const response = await axios.post('API ROUTE FOR LOGIN', {
                 username,
                 password
             });
@@ -30,34 +44,49 @@ class LoginPage extends Component {
 
             // Reset Form Fields
             this.setState({username: '', password: ''})
+        
         } catch (error) {
             console.error('Error Logging User In: ', error.response.data.message);
+            this.setState({ errors: { apiError: error.response.data.message } });
         }
+
     }
 
     render() {
         const username = this.state.username;
         const password = this.state.password;
+        const errors = this.state.errors;
         return (
             <div>
+                
                 <form>
+                    
                     <InputField
                         label="Username"
                         type="text"
                         name="username"
                         value={username}
                         onChange={this.handleChange}
-                        style={{}}
+                        style={errors.username ? {borderColor: 'red'} : {}}
                     />
+                    {errors.username && <div>{errors.username}</div>}
+                    
                     <InputField 
                         label="Password"
                         type="password"
                         name="password"
                         value={password}
                         onChange={this.handleChange}
-                        style={{}}
+                        style={errors.password ? {borderColor: 'red'} : {}}
                     />
+                    {errors.password && <div>{errors.password}</div>}
+                    
+                    {errors.apiError && <div>{errors.apiError}</div>}
+                    
+                    <button type='submit'>Login In</button>
+                
                 </form>
+            
             </div>
         );
     };
