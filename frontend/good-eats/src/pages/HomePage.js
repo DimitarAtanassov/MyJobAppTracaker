dc0fb4e0a3cf0bf9907dd8ff4e9499e572045b94
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-11 16:16:40 -0700

    Frontend Add: Google Map Component and .env w/ API key

--- frontend/good-eats/src/pages/HomePage.js
+++ frontend/good-eats/src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import GoogleMapComp from '../components/GoogleMapComp';
+const spotLocation = { lat: 40.7128, lng: -74.006 };
 const HomePage = () => {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>Find a spot</h1>
+            <GoogleMapComp spotLocation={spotLocation} />
         </>
     )
 }
