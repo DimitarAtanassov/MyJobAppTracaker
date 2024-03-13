08cefc53d4744e06ab23982d700f9b85ec7ea41e
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-13 09:58:03 -0700

    Frontend add: Comments

--- frontend/good-eats/src/components/LoginRegisterButton.js
+++ frontend/good-eats/src/components/LoginRegisterButton.js
@@ -1,5 +1,6 @@
 // LoginRegisterButton.js
 /* React Component, Button to redirect a User between SignUp and Login Pages*/
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
